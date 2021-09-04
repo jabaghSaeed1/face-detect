4a8276edcce620e31a229e8758d7ed8ca539e5c0
jabagh saeed <jabaghsaeedjabagh@gmail.com>
2021-09-04 22:08:04 +0300

    Updates

---
+++ precache-manifest.be2aabea1455a9547f7e1e4ce6455cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829e85167c347ef060750688c74e11a2",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "0974f24f668d7c086a1f",
+    "url": "/face-detect/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "8ca2265af6215e58dcaf",
+    "url": "/face-detect/static/css/main.7dc70458.chunk.css"
+  },
+  {
+    "revision": "0974f24f668d7c086a1f",
+    "url": "/face-detect/static/js/2.02358d25.chunk.js"
+  },
+  {
+    "revision": "cbe01fbbe035bf90d35a835621f136ef",
+    "url": "/face-detect/static/js/2.02358d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca2265af6215e58dcaf",
+    "url": "/face-detect/static/js/main.eed0a072.chunk.js"
+  },
+  {
+    "revision": "222e7e7060c17ee90085",
+    "url": "/face-detect/static/js/runtime-main.e8a0c4bc.js"
+  }
+]);
